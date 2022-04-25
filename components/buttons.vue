99b3c9aee7651ecc296216b95f439fbd3898bb17
ivesal1 <mahdijafari.v@gmail.com>
2022-04-25 16:44:54 +0430

    api is deployed

---
+++ components/buttons.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+      <button class="p-2 bg-black">salam</button>
+      <div v-for="button in buttons" :key="button.data">
+          <div>{{button.code}}</div>
+      </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data(){
+        return {
+            buttons:[]
+        }
+    },
+    // async asyncData() {
+    //     const buttons = await axios.get('https://tandori.ir/plus/v1/categories')
+    //     return {
+    //         buttons : buttons
+    //     }
+    // },
+    created(){
+        this.fetchSomething().then(res => {
+            this.buttons = res.data.data
+            console.log(this.buttons);
+        });
+    },
+    methods : {
+        fetchSomething () {
+            try {
+                return axios.get('https://tandori.ir/plus/v1/categories'); 
+            } catch (error) {
+                console.error(error);
+                console.log(this.buttons);
+            }
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
