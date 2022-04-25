1fc3b2db035b482e3fef5251cea481b20c3697cd
ivesal1 <mahdijafari.v@gmail.com>
2022-04-25 18:18:42 +0430

    underline under titles created

---
+++ components/underLine.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="underLineBaba">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .underLineBaba{
+        height: 3px;
+        width: 200px;
+        background-image: linear-gradient(to left , #b43125 , #111);
+    }
+</style>
