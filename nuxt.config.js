6b378e3418512b32db72186741c504416ab5fdc9
ivesal1 <mahdijafari.v@gmail.com>
2022-04-25 16:45:29 +0430

    axios is added to project

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,6 +24,7 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
+    '@nuxtjs/axios'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
