f6a860f19e5eb0e1ea5bb03aa32c7b869a587619
ivesal1 <mahdijafari.v@gmail.com>
2022-04-27 00:14:17 +0430

    component is completed

---
+++ pages/bread/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container mx-auto">
+      <buttons/>
+      <bread/>
+  </div>
+</template>
+
+<script>
+import bread from '~/components/products/bread'
+export default {
+components:{
+    bread
+}
+}
+</script>
+
+<style>
+
+</style>
