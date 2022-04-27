45087e36975318ecc54c7c26a755350b8620c368
ivesal1 <mahdijafari.v@gmail.com>
2022-04-27 13:09:13 +0430

    mobile button component is created

---
+++ components/mobileButtons.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data(){
+        return {
+            buttons:[]
+        }
+    },
+    created(){
+        this.fetchSomething().then(res => {
+            this.buttons = res.data.data
+            console.log(this.buttons);
+        });
+    },
+    methods : {
+        fetchSomething () {
+            try {
+                return axios.get('https://tandori.ir/plus/v1/categories'); 
+            } catch (error) {
+                console.error(error);
+                console.log(this.buttons);
+            }
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
